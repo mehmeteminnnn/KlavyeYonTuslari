07313f9b6c2b8c5e06a10638425f6960114586ac
Mehmet Emin TOK <95161664+mehmeteminnnn@users.noreply.github.com>
2022-11-10 00:34:27 +0300

    KlavyeİleHareket
    C#'ta klavye yön tuşları ile pictureboxı hareket ettirme

---
+++ KlavyeİleHareket/KlavyeİleHareket/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace KlavyeİleHareket
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::KlavyeİleHareket.Properties.Resources.rolling_football_ball_gif;
+            this.pictureBox1.Location = new System.Drawing.Point(148, 71);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(156, 116);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.label1.Location = new System.Drawing.Point(412, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 273);
+            this.label1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.Desktop;
+            this.label2.Location = new System.Drawing.Point(31, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 273);
+            this.label2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.Desktop;
+            this.label3.Location = new System.Drawing.Point(31, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(401, 26);
+            this.label3.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.Desktop;
+            this.label4.Location = new System.Drawing.Point(31, 341);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(401, 26);
+            this.label4.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(508, 460);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ KlavyeİleHareket/KlavyeİleHareket/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KlavyeİleHareket
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int x = pictureBox1.Location.X;
+            int y = pictureBox1.Location.Y;
+            if (e.KeyCode == Keys.Right&&x<300
+                )
+            {
+                x = x + 5;
+
+            }
+            if (e.KeyCode == Keys.Left&& x>0)
+            {
+                x = x-5;
+            }
+                if (e.KeyCode == Keys.Up&&y>0)
+                {
+                    y = y -5;
+
+                }
+                if (e.KeyCode == Keys.Down&&y<300)
+                {
+                    y += 5;
+                }
+                if(pictureBox1.Right>=label1.Left)
+            {
+                label1.BackColor = Color.Red;
+            }
+            if (pictureBox1.Left <= label2.Right)
+            {
+                label2.BackColor = Color.Yellow;
+            }
+            if(pictureBox1.Bottom>=label4.Top)
+                {
+                label4.BackColor= Color.Green;
+            }
+            if (pictureBox1.Top <= label3.Bottom)
+            {
+                label3.BackColor = Color.Peru;
+            }
+            pictureBox1.Location = new Point(x,y);
+                
+                ;
+            }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+
